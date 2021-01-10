b3ce272a42534702dd1f505b9a0445d78ffb2a9d
weshy007 <josephwaweru96@gmail.com>
2021-01-10 18:54:29 +0300

    functional code added

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-// $(document).ready(function() {
-//     $(".mouse").click(function() {
-//       $(".formzi").slideToggle();
-//       $(".formzi").slideToggle();
-//     });
-
 var OrderNew = [];
 function Pizza(pizzaSize, pizzaCrust, pizzaTopping, pizzaNumber) {
     this.pizzaSize = pizzaSize;
@@ -53,10 +47,10 @@ Pizza.prototype.pizzaTp =function(){
 
 $("#checkout").click(function (event) {
     
-    var PizzaSize = parseInt( $(".sizey").val());
-    var PizzaCrust = parseInt( $(".crusty").val());
-    var PizzaTopping =parseInt( $(".toppings").val());
-    var PizzaNumber =parseInt( $(".numberz").val());
+    var PizzaSize = parseInt( $("#size").val());
+    var PizzaCrust = parseInt( $("#crust").val());
+    var PizzaTopping =parseInt( $("#topping").val());
+    var PizzaNumber =parseInt( $("#number").val());
     var newPizzae = new Pizza(PizzaSize, PizzaCrust, PizzaTopping, PizzaNumber); 
     
     console.log(newPizzae)
@@ -64,4 +58,80 @@ $("#checkout").click(function (event) {
     OrderNew.push(newPizzae);
     console.log(OrderNew)
 
-});
+    $("#size").val("");
+    $("#crust").val("");
+    $("#topping").val("");
+    $("#number").val("");
+     
+    totalAmount = 0
+    for (let i = 0; i < OrderNew.length; i++ ){
+        totalAmount += OrderNew[i].totalPrice();
+        console.log(totalAmount)
+    }
+    
+    $("#ordersTaken").append("<tr>" + '<td scope="orderCalculation">' + 
+    newPizzae.pizzaSz () + newPizzae.pizzaSize + "</td>" + "<td>" +
+    newPizzae.pizzaCr () + newPizzae.pizzaCrust + "</td>" + "<td>" +
+    newPizzae.pizzaTp () + newPizzae.pizzaTopping + "</td>" + "<td>" + 
+    newPizzae.PizzaNumber + "</td>" + "<td>" +
+    newPizzae.totalPrice() + "</td>" + "</tr>"
+    );
+
+    $("#ordersTaken").append("");
+    if (OrderNew.length > 0) {
+        $("#form-heading").empty();
+        $("#form-heading").append("Make A New Order");
+    }
+
+    $("#totalAmount").fadeIn();
+    $("#Checkout").fadeIn();
+    $("#orderClass").fadeIn();
+    $("#totalAmount").empty();
+    $("#totalAmount").append(totalAmount);
+    $("#totalAmount").show();
+  });
+
+  $("#Checkout").click(function () {
+  $(".checkout-info").show();
+  });
+
+  $("#checkoutForm").submit(function (event) {
+  event.preventDefault();
+  var name = $("#name").val();
+  var deliveryOption = $("#OptionDelivery").val();
+  NameOfCustomer = name;
+  $("#name").val("");
+  $("#OptionDelivery").val("");
+  $(".checkout-info").hide();
+  $("#Checkout").hide();
+  $("#totalAmount").empty();
+  
+  if (deliveryOption === "deliver") {
+    $(".deliveryLocation").show();
+    $(".deliveryCost").show();
+    $("#totalAmount").empty();
+    $("#delivery-cost").append(250);
+    totalAmount += 250;
+    $("#totalAmount").empty();
+    $("#totalAmount").empty();
+  $("#totalAmount").append(totalAmount);
+  $(".TotalAmount").show();
+  } 
+  else {
+    alert(+"Dear Customer, " + ": Your total bill is Ksh. " + totalAmount + ".Collect your order in the next 20 minutes." + " Your Tag Name is " + NameOfCustomer);
+  }
+  });
+
+  $("#locationForm").submit(function (event) {
+  event.preventDefault();
+  var locationEntered = $("location").val();
+  var mobileEntered = $("#mobilenumb").val();
+  location = locationEntered;
+  mobileNumb = mobileEntered;
+  $("#Checkout").hide();
+  $(".deliveryLocation").hide();
+  $("#totalAmount").empty();
+  $("#totalAmount").append(totalAmount);
+  $(".TotalAmount").show();
+  alert("Dear Customer"  + ": Your new total bill is Ksh. " + totalAmount + ". Your order will be delivered in the next one hour." + " Payment on delivery!!.");
+  });
